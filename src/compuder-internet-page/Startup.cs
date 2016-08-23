76d66727ca9b4981a660e9db88fcf3f88a4a56b0
okcompuder <dgrujo@gmail.com>
2016-08-23 01:10:12 -0500

    Initial Code Commit

---
+++ src/compuder-internet-page/Startup.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace compuder_internet_page
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services) { }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            DefaultFilesOptions options = new DefaultFilesOptions();
+            options.DefaultFileNames.Clear();
+            options.DefaultFileNames.Add("html/index.html");
+            app.UseDefaultFiles(options);
+            app.UseStaticFiles();
+        }
+    }
+}
---
+++ src/compuder-internet-page/wwwroot/js/site.js
@@ -0,0 +1,7 @@
+﻿function mobileHoverFix() {
+    var el = this;
+    var par = el.parentNode;
+    var next = el.nextSibling;
+    par.removeChild(el);
+    setTimeout(function () { par.insertBefore(el, next); }, 0)
+}
